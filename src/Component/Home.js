5ecb377170e39341eab713bf52f2e4a063713c90
nmrayhan <nurmdrayhan97@gmail.com>
2022-07-07 10:33:20 -0700

    navbar component

---
+++ src/Component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './common/Navbar';
+
+const Home = () => {
+    return (
+        <div className='w-9/12 mx-auto'>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default Home;
